3c680910fc35d6740518c66a4a5da3b12dd3602e
midorigreen <stitow14@gmail.com>
2017-09-21 21:51:41 +0900

    Add proxy server

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+const (
+	host = "https://api.github.com"
+)
+
+func main() {
+	http.HandleFunc("/", proxyHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func proxyHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.URL)
+	url := fmt.Sprintf("%s%s", host, r.URL)
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	defer resp.Body.Close()
+
+	byteBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprint(w, string(byteBody))
+}
