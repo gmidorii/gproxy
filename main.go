4d2fe89f282472cf14b9b6eaff0f47cde6eb1ba2
tagasouichiro <stitow14@gmail.com>
2017-09-23 12:31:23 +0900

    Update GET request to using channel

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -9,6 +11,11 @@ import (
 )
 
 var proto = "http"
+
+type response struct {
+	Resp *http.Response
+	Err  error
+}
 
 func main() {
 	http.HandleFunc("/", proxyHandler)
@@ -23,19 +30,9 @@ func proxyHandler(w http.ResponseWriter, r *http.Request) {
 	host := r.URL.Query().Get("cors-host")
 	url := fmt.Sprintf("%s://%s%s", proto, host, r.URL)
 
-	client := &http.Client{
-		Timeout: 5 * time.Second,
-	}
-	resp, err := client.Get(url)
-	if err != nil {
-		log.Println(err)
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprint(w, "Error")
-		return
-	}
-	defer resp.Body.Close()
-
-	byteBody, err := ioutil.ReadAll(resp.Body)
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+	body, err := corsGet(ctx, url)
 	if err != nil {
 		log.Println(err)
 		w.WriteHeader(http.StatusInternalServerError)
@@ -45,5 +42,41 @@ func proxyHandler(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.WriteHeader(http.StatusOK)
-	fmt.Fprint(w, string(byteBody))
+	fmt.Fprint(w, body)
+}
+
+func corsGet(ctx context.Context, url string) (string, error) {
+	tr := &http.Transport{}
+	client := &http.Client{Transport: tr}
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return "", err
+	}
+
+	ch := make(chan response, 1)
+	go func() {
+		resp, err := client.Do(req)
+		ch <- response{Resp: resp, Err: err}
+	}()
+
+	select {
+	case <-ctx.Done():
+		tr.CancelRequest(req)
+		<-ch
+		log.Printf("cancel request %s\n", req.URL.RequestURI())
+		return "", ctx.Err()
+	case res := <-ch:
+		if res.Err != nil {
+			return "", res.Err
+		}
+		defer res.Resp.Body.Close()
+
+		byteBody, err := ioutil.ReadAll(res.Resp.Body)
+		if err != nil {
+			return "", err
+		}
+		return string(byteBody), nil
+	}
+	return "", errors.New("not expected reached")
 }
